93f82229feb676f88a98036d4cec5a1f2e1258a8
Pankarhoni Patton <pankarhoni@gmail.com>
2024-11-26 15:02:19 +0530

    Update Shop.jsx

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import Hero from '../Components/Hero/Hero'
 import NewCollections from '../Components/NewCollections/NewCollections'
+import { API_URL } from "../config"; // Importing API_URL
 
 const Shop = () => {
 
   const [newcollection, setNewCollection] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/newcollections') 
+    fetch('${API_URL}/newcollections') 
             .then((res) => res.json()) 
             .then((data) => setNewCollection(data))
     }
