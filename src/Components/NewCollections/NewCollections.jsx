e951a73cbfa57f967b277c49f98db5ad1e3f2a1c
Pankarhoni Patton <pankarhoni@gmail.com>
2024-11-26 16:00:22 +0530

    Update NewCollections.jsx

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,7 +1,7 @@
 import React,{useEffect,useState} from 'react'
 import './NewCollections.css'
 import Item from '../Item/Item'
-import { API_URL } from "../../../config"; // One more directory up, just in case
+ const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 
 const NewCollections = (props) => {
